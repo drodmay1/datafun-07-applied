a1c2d71f50761d31de5257cc7f330c837f6c0456
David <drodmay@gmail.com>
2024-02-11 14:16:35 -0500

    added

--- Chapter10 data/10.6 Card Shuffling.py
+++ Chapter10 data/10.6 Card Shuffling.py
@@ -1,6 +1,12 @@
 from deck import DeckOfCards
-DeckOfCards = DeckOfCards()
-print(deck_of_cards)
+import shuffle
+
+deck_of_cards = DeckOfCards()
+
+deck_of_cards.shuffle()
+print(DeckOfCards)
+
+
 
     
 
