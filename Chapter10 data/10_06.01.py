bc29d25a3c2a6e8466b02623d1b08ada2dddb132
David <drodmay@gmail.com>
2024-02-11 14:28:38 -0500

    10.6.1 added

--- Chapter10 data/10_06.01.py
+++ Chapter10 data/10_06.01.py
@@ -1,22 +1,23 @@
+# Section 10.6.1 snippets
+
+# Creating, Shuffling and Dealing the Cards 
 from deck import DeckOfCards
 
 deck_of_cards = DeckOfCards()
 
-deck_of_cards.shuffle()
 print(deck_of_cards)
 
+deck_of_cards.shuffle()
+
+print(deck_of_cards)
+
+# Dealing Cards
 deck_of_cards.deal_card()
 
+# Class Card’s Other Features
 card = deck_of_cards.deal_card()
+
 str(card)
-print(card)
 
 card.image_name
-
-
-
-
-
-
-    
 
